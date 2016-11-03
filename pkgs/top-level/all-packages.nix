84fae86aa51c6fd54bddcc6b03160f32d57e7a4a
Rhys <rimmington@gmail.com>
2016-11-03 16:32:29 +1100

    openzwave: add package (version 1.4.164)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2945,6 +2945,8 @@ in
 
   olsrd = callPackage ../tools/networking/olsrd { };
 
+  openzwave = callPackage ../development/libraries/openzwave { };
+
   ossec = callPackage ../tools/security/ossec {};
 
   ostree = callPackage ../tools/misc/ostree { };
